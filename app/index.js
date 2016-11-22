f1e038966e10347b471d57d11c29ad1f68bfac2e
Jessica Stokes <hello@jessicastokes.net>
2016-11-22 12:37:14 +1100

    Turn on Type Snob

--- app/index.js
+++ app/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
 
+// Toggle on development features
+if (process.env.NODE_ENV !== "production") {
+  require('react-type-snob').default(React);
+}
+
 ReactDOM.render(Routes, document.getElementById('root'));
