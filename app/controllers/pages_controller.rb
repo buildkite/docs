57ebcc4824a3625d311dfd604ca17633c5423cfc
Keith Pitt <me@keithpitt.com>
2019-03-07 16:19:27 +0800

    Fixed page titles and canonical redirection

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,17 +5,19 @@ class PagesController < ApplicationController
     # @docs_presenter = DocumentationPresenter.new(request)
     @page = Page.new(view_context, params[:path])
 
-    unless @page.exists?
-      raise ActionController::RoutingError.new("That documentation page does not exist")
+    # If the page doesn't exist, throw a 404
+    raise ActionController::RoutingError.new("That documentation page does not exist") unless @page.exists?
+
+    # If the URL we landed on is the *correct* version of the URL, render the
+    # content.
+    #
+    # If there's another more correct version of the URL (i.e. we changed `_`
+    # to `-`), then redirect them to where they should be.
+    if @page.is_canonical?
+      @page_title = @page.title
+      render html: @page.body, layout: true
+    else
+      redirect_to "/docs/#{@page.canonical_url}", status: :moved_permanently
     end
-
-    view_context.provide(:page_title, @page.title)
-
-    render html: @page.body, layout: true
-
-
-    # unless @docs_page_presenter.is_canonical?
-    #   redirect_to @docs_presenter.url_for(@docs_page_presenter.canonical_url), status: :moved_permanently
-    # end
   end
 end
