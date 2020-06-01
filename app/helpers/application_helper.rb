a2f4a2c02dd3134b58f4e4875f45807a19879415
Fred Showell <fred@buildkite.com>
2020-06-01 11:36:01 +1000

    Add <li> to sidebar link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,8 +9,9 @@ module ApplicationHelper
     options[:class] = [options[:class]].flatten.compact
     options[:class] << 'Docs__nav__sub-nav__item__link Link--on-white Link--no-underline'
     options[:class] << "active" if current_page?(full_path)
+    link = link_to name, full_path, options
 
-    link_to name, full_path, options
+    content_tag(:li, link, class: 'Docs__nav__sub-nav__item')
   end
 
   def open_source_url
