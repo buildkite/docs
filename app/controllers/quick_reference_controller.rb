cb94fa375836fe67e61c458d9bb314b5dc52babe
Grant Colegate <blaknite@thelanbox.com.au>
2020-06-15 12:43:47 +0930

    Add CORS headers to quick-reference JSON

--- app/controllers/quick_reference_controller.rb
+++ app/controllers/quick_reference_controller.rb
@@ -1,3 +1,12 @@
 class QuickReferenceController < ApplicationController
+  before_action :add_cors_headers
+
   def pipelines; end
+
+  private
+
+  def add_cors_headers
+    headers["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+    headers["Access-Control-Allow-Origin"] = '*'
+  end
 end
