bce4874bcd728d01596f062b3b3befb7a29cb94a
Keith Duncan <keith.d@buildkite.com>
2021-09-29 10:59:50 +1000

    Store the aws-stack.yml in data

--- scripts/update-elastic-ci-stack-for-aws-parameters.sh
+++ scripts/update-elastic-ci-stack-for-aws-parameters.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 template_url="${1:-https://s3.amazonaws.com/buildkite-aws-stack/latest/aws-stack.yml}"
-output_path="pages/agent/v3/elastic_ci_aws/aws-stack.yml"
+output_path="data/aws-stack.yml"
 
 echo "Fetching template from ${template_url} to ${output_path}" >&2
 curl --fail --silent --show-error --location "${template_url}" > "${output_path}"
