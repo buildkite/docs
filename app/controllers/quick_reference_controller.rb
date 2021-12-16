2306366044ea757f2ce188c8ae5b1d1d89c409de
Plaindocs <sam.w@buildkite.com>
2021-12-16 21:40:35 +0000

    Update app/controllers/quick_reference_controller.rb

--- app/controllers/quick_reference_controller.rb
+++ app/controllers/quick_reference_controller.rb
@@ -8,8 +8,7 @@ class QuickReferenceController < ApplicationController
     'pipelines/wait_step',
     'pipelines/block_step',
     'pipelines/input_step',
-    'pipelines/trigger_step',
-    'pipelines/managing-priorities'
+    'pipelines/trigger_step'
   ].freeze
 
   NOTIFICATION_PAGES = [
