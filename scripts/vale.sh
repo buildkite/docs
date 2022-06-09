45d95ccf83d0d6849860f243dcff716680d5080b
Daniel D. Beck <daniel@ddbeck.com>
2022-06-09 15:36:43 +0100

    Move _bootstrap.md-specific Vale config out of shell script

--- scripts/vale.sh
+++ scripts/vale.sh
@@ -8,9 +8,5 @@ fi
 
 DOCS_ROOT=$(dirname "$(dirname "$(realpath "$0")")")
 
-# TODO: Reinstate linting of pages/agent/v3/help/_bootstrap.md
-# The file contains an unresolvable spelling error (because the genuine source of the file lives outside this repo).
-# When the spelling error is fixed, remove the `--glob` option to reinstate linting.
-# See https://github.com/buildkite/docs/pull/1585/#discussion_r893429064 and https://github.com/buildkite/agent/pull/1672.
-vale --config "$DOCS_ROOT"/vale/.vale.ini --glob='!*/pages/agent/v3/help/_bootstrap.md' "$DOCS_ROOT"/pages &&
+vale --config "$DOCS_ROOT"/vale/.vale.ini "$DOCS_ROOT"/pages &&
   vale --config "$DOCS_ROOT/vale/.vale.snippets.ini" "$DOCS_ROOT"/pages/**/_*
