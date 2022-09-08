a38a689a13ca1e8538f3817e1c2a6a2139afed96
Oly Su <oly@buildkite.com>
2022-09-08 22:47:57 +1000

    ✅ Update nav_data_spec

--- spec/scripts/graphql_api_content/nav_data_spec.rb
+++ spec/scripts/graphql_api_content/nav_data_spec.rb
@@ -5,66 +5,244 @@ RSpec.describe NavData do
   type_sets = {
     "query_types" => [
       {
-        "name" => "schema1"
+        "name" => "agent",
+        "description" => "Find an agent by its slug",
+        "args" => [
+          {
+            "name" => "slug",
+            "description" => "The UUID for the agent, prefixed by its organization's slug i.e. `acme-inc/0bd5ea7c-89b3-4f40-8ca3-ffac805771eb`",
+            "type" => {
+              "kind" => "NON_NULL",
+              "name" => nil,
+              "ofType" => {
+                "kind" => "SCALAR",
+                "name" => "ID",
+                "ofType" => nil
+              }
+            },
+            "defaultValue" => nil
+          }
+        ],
+        "type" => {
+          "kind" => "OBJECT",
+          "name" => "Agent",
+          "ofType" => nil
+        },
+        "isDeprecated" => false,
+        "deprecationReason" => nil
       },
       {
-        "name" => "schema2"
+        "name" => "agentToken",
+        "description" => "Find an agent token by its slug",
+        "args" => [
+          {
+            "name" => "slug",
+            "description" => "The UUID for the agent token, prefixed by its organization's slug i.e. `acme-inc/0bd5ea7c-89b3-4f40-8ca3-ffac805771eb`",
+            "type" => {
+              "kind" => "NON_NULL",
+              "name" => nil,
+              "ofType" => {
+                "kind" => "SCALAR",
+                "name" => "ID",
+                "ofType" => nil
+              }
+            },
+            "defaultValue" => nil
+          }
+        ],
+        "type" => {
+          "kind" => "OBJECT",
+          "name" => "AgentToken",
+          "ofType" => nil
+        },
+        "isDeprecated" => false,
+        "deprecationReason" => nil
       }
     ],
     "mutation_types" => [
       {
-        "name" => "mutationSchema1"
+        "name" => "agentStop",
+        "description" => "Instruct an agent to stop accepting new build jobs and shut itself down.",
+        "args" => [
+          {
+            "name" => "input",
+            "description" => nil,
+            "type" => {
+              "kind" => "NON_NULL",
+              "name" => nil,
+              "ofType" => {
+                "kind" => "INPUT_OBJECT",
+                "name" => "AgentStopInput",
+                "ofType" => nil
+              }
+            },
+            "defaultValue" => nil
+          }
+        ],
+        "type" => {
+          "kind" => "OBJECT",
+          "name" => "AgentStopPayload",
+          "ofType" => nil
+        },
+        "isDeprecated" => false,
+        "deprecationReason" => nil
       },
       {
-        "name" => "mutationSchema2"
+        "name" => "agentTokenCreate",
+        "description" => "Create a new agent registration token.",
+        "args" => [
+          {
+            "name" => "input",
+            "description" => nil,
+            "type" => {
+              "kind" => "NON_NULL",
+              "name" => nil,
+              "ofType" => {
+                "kind" => "INPUT_OBJECT",
+                "name" => "AgentTokenCreateInput",
+                "ofType" => nil
+              }
+            },
+            "defaultValue" => nil
+          }
+        ],
+        "type" => {
+          "kind" => "OBJECT",
+          "name" => "AgentTokenCreatePayload",
+          "ofType" => nil
+        },
+        "isDeprecated" => false,
+        "deprecationReason" => nil
       }
     ],
     "object_types" => [
       {
-        "name" => "schema3"
+        "kind" => "OBJECT",
+        "name" => "Avatar",
+        "description" => "An avatar belonging to a user",
+        "fields" => [],
+        "inputFields" => nil,
+        "interfaces" => [],
+        "enumValues" => nil,
+        "possibleTypes" => nil
       },
       {
-        "name" => "schema4"
+        "kind" => "OBJECT",
+        "name" => "PullRequest",
+        "description" => "A pull request on a provider",
+        "fields" => [],
+        "inputFields" => nil,
+        "interfaces" => [],
+        "enumValues" => nil,
+        "possibleTypes" => nil
       }
     ],
     "scalar_types" => [
       {
-        "name" => "schema5"
+        "kind" => "SCALAR",
+        "name" => "Boolean",
+        "description" => "Represents `true` or `false` values.",
+        "fields" => nil,
+        "inputFields" => nil,
+        "interfaces" => nil,
+        "enumValues" => nil,
+        "possibleTypes" => nil
       },
       {
-        "name" => "schema6"
+        "kind" => "SCALAR",
+        "name" => "String",
+        "description" => "Represents textual data as UTF-8 character sequences. This type is most often used by GraphQL to represent free-form human-readable text.",
+        "fields" => nil,
+        "inputFields" => nil,
+        "interfaces" => nil,
+        "enumValues" => nil,
+        "possibleTypes" => nil
       }
     ],
     "interface_types" => [
-      {
-        "name" => "schema7"
+      {          
+        "kind" => "INTERFACE",
+        "name" => "Node",
+        "description" => "An object with an ID.",
+        "inputFields" => nil,
+        "interfaces" => nil,
+        "enumValues" => nil
       },
       {
-        "name" => "schema8"
+        "kind" => "INTERFACE",
+        "name" => "Connection",
+        "description" => nil,
+        "fields" => [],
+        "inputFields" => nil,
+        "interfaces" => nil,
+        "enumValues" => nil,
+        "possibleTypes" => []
       }
     ],
     "enum_types" => [
       {
-        "name" => "schema9"
+        "kind" => "ENUM",
+        "name" => "BuildBlockedStates",
+        "description" => "All the possible blocked states a build can be in",
+        "fields" => nil,
+        "inputFields" => nil,
+        "interfaces" => nil,
+        "enumValues" => [],
+        "possibleTypes" => nil
       },
       {
-        "name" => "schema10"
+        "kind" => "ENUM",
+        "name" => "PipelineVisibility",
+        "description" => "The visibility of the pipeline",
+        "fields" => nil,
+        "inputFields" => nil,
+        "interfaces" => nil,
+        "enumValues" => [],
+        "possibleTypes" => nil
       }
     ],
     "input_object_types" => [
       {
-        "name" => "schema11"
+        "kind" => "INPUT_OBJECT",
+        "name" => "JobConcurrencySearch",
+        "description" => "Searching for concurrency groups on jobs",
+        "fields" => nil,
+        "inputFields" => [],
+        "interfaces" => nil,
+        "enumValues" => nil,
+        "possibleTypes" => nil
       },
       {
-        "name" => "schema12"
+        "kind" => "INPUT_OBJECT",
+        "name" => "JobStepSearch",
+        "description" => "Searching for jobs based on step information",
+        "fields" => nil,
+        "inputFields" => [],
+        "interfaces" => nil,
+        "enumValues" => nil,
+        "possibleTypes" => nil
       }
     ],
     "union_types" => [
       {
-        "name" => "schema13"
+        "kind" => "UNION",
+        "name" => "BuildCreator",
+        "description" => "Either a `User` or an `UnregisteredUser` type",
+        "fields" => nil,
+        "inputFields" => nil,
+        "interfaces" => nil,
+        "enumValues" => nil,
+        "possibleTypes" => []
       },
       {
-        "name" => "schema14"
+        "kind" => "UNION",
+        "name" => "Job",
+        "description" => "Kinds of jobs that can exist on a build",
+        "fields" => nil,
+        "inputFields" => nil,
+        "interfaces" => nil,
+        "enumValues" => nil,
+        "possibleTypes" => []
       }
     ]
   }
@@ -73,12 +251,12 @@ RSpec.describe NavData do
     it "converts to an array of nav_item hashes" do
       expect(convert_to_nav_items(type_sets["object_types"])).to eq([
         {
-          "name" => "schema3",
-          "path" => "apis/graphql/schemas//schema3"
+          "name" => "Avatar",
+          "path" => "apis/graphql/schemas/object/avatar"
         },
         {
-          "name" => "schema4",
-          "path" => "apis/graphql/schemas//schema4"
+          "name" => "PullRequest",
+          "path" => "apis/graphql/schemas/object/pullrequest"
         }
       ])
     end
@@ -143,12 +321,12 @@ RSpec.describe NavData do
                     "name" => "Queries",
                     "children" => [
                       {
-                        "name" => "schema1",
-                        "path" => "apis/graphql/schemas//schema1"
+                        "name" => "agent",
+                        "path" => "apis/graphql/schemas/query/agent"
                       },
                       {
-                        "name" => "schema2",
-                        "path" => "apis/graphql/schemas//schema2"
+                        "name" => "agentToken",
+                        "path" => "apis/graphql/schemas/query/agenttoken"
                       }
                     ]
                   },
@@ -156,32 +334,38 @@ RSpec.describe NavData do
                     "name" => "Mutations",
                     "children" => [
                       {
-                        "name" => "mutationSchema1",
-                        "path" => "apis/graphql/schemas//mutationschema1"
+                        "name" => "agentStop",
+                        "path" => "apis/graphql/schemas/mutation/agentstop"
                       },
                       {
-                        "name" => "mutationSchema2",
-                        "path" => "apis/graphql/schemas//mutationschema2"
+                        "name" => "agentTokenCreate",
+                        "path" => "apis/graphql/schemas/mutation/agenttokencreate"
                       }
                     ]
                   },
                   {
                     "name" => "Objects",
                     "children" => [
-                      {"name" => "schema3", "path" => "apis/graphql/schemas//schema3"},
-                      {"name" => "schema4", "path" => "apis/graphql/schemas//schema4"}
+                      {
+                        "name" => "Avatar",
+                        "path" => "apis/graphql/schemas/object/avatar"
+                      },
+                      {
+                        "name" => "PullRequest",
+                        "path" => "apis/graphql/schemas/object/pullrequest"
+                      }
                     ]
                   },
                   {
                     "name" => "Scalars",
                     "children" => [
                       {
-                        "name" => "schema5",
-                        "path" => "apis/graphql/schemas//schema5"
+                        "name" => "Boolean",
+                        "path" => "apis/graphql/schemas/scalar/boolean"
                       },
                       {
-                        "name" => "schema6",
-                        "path" => "apis/graphql/schemas//schema6"
+                        "name" => "String",
+                        "path" => "apis/graphql/schemas/scalar/string"
                       }
                     ]
                   },
@@ -189,12 +373,12 @@ RSpec.describe NavData do
                     "name" => "Interfaces",
                     "children" => [
                       {
-                        "name" => "schema7",
-                        "path" => "apis/graphql/schemas//schema7"
+                        "name" => "Connection",
+                        "path" => "apis/graphql/schemas/interface/connection"
                       },
                       {
-                        "name" => "schema8",
-                        "path" => "apis/graphql/schemas//schema8"
+                        "name" => "Node",
+                        "path" => "apis/graphql/schemas/interface/node"
                       }
                     ]
                   },
@@ -202,12 +386,12 @@ RSpec.describe NavData do
                     "name" => "ENUMs",
                     "children" => [
                       {
-                        "name" => "schema10",
-                        "path" => "apis/graphql/schemas//schema10"
+                        "name" => "BuildBlockedStates",
+                        "path" => "apis/graphql/schemas/enum/buildblockedstates"
                       },
                       {
-                        "name" => "schema9",
-                        "path" => "apis/graphql/schemas//schema9"
+                        "name" => "PipelineVisibility",
+                        "path" => "apis/graphql/schemas/enum/pipelinevisibility"
                       }
                     ]
                   },
@@ -215,12 +399,12 @@ RSpec.describe NavData do
                     "name" => "Input objects",
                     "children" => [
                       {
-                        "name" => "schema11",
-                        "path" => "apis/graphql/schemas//schema11"
+                        "name" => "JobConcurrencySearch",
+                        "path" => "apis/graphql/schemas/input-object/jobconcurrencysearch"
                       },
                       {
-                        "name" => "schema12",
-                        "path" => "apis/graphql/schemas//schema12"
+                        "name" => "JobStepSearch",
+                        "path" => "apis/graphql/schemas/input-object/jobstepsearch"
                       }
                     ]
                   },
@@ -228,12 +412,12 @@ RSpec.describe NavData do
                     "name" => "Unions",
                     "children" => [
                       {
-                        "name" => "schema13",
-                        "path" => "apis/graphql/schemas//schema13"
+                        "name" => "BuildCreator",
+                        "path" => "apis/graphql/schemas/union/buildcreator"
                       },
                       {
-                        "name" => "schema14",
-                        "path" => "apis/graphql/schemas//schema14"
+                        "name" => "Job",
+                        "path" => "apis/graphql/schemas/union/job"
                       }
                     ]
                   }
