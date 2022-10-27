a60b11c31e96df1bba98f58907a52b026cb1f35e
Elle Meredith <ellemeredith@gmail.com>
2022-10-27 18:58:47 +1100

    Define index action for homepage
    - To remove the confusion of where the home page is defined,
      making it easier to understand what is happening
    - Also removing one branch in the layout conditional

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,15 @@ class PagesController < ApplicationController
   append_view_path "pages"
   layout :layout_by_path
 
+  def index
+    render :index, layout: "homepage"
+  end
+
   def show
     @page = Page.new(view_context, params[:path])
 
     # If the page doesn't exist, throw a 404
     raise ActionController::RoutingError.new("The documentation page `#{@page.basename}` does not exist") unless @page.exists?
-
-    # For the homepage, render with a custom layout that doesn't include the sidebar etc
 
     # If there's another more correct version of the URL (for example, we changed `_`
     # to `-`), then redirect them to where they should be.
@@ -32,9 +34,7 @@ class PagesController < ApplicationController
   helper_method :is_landing_page?
 
   def layout_by_path
-    if request.path == "/docs"
-      "homepage"
-    elsif request.path.starts_with? "/docs/apis/graphql"
+    if request.path.starts_with? "/docs/apis/graphql"
       "graphql"
     else
       "application"
