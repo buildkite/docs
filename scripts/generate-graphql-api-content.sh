1bb3abb2a8a7d8f113efd8f1b706c00766de2c09
Chris Campbell <chris.c@buildkite.com>
2023-02-06 17:45:51 +1100

    adding some logging

--- scripts/generate-graphql-api-content.sh
+++ scripts/generate-graphql-api-content.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 scripts_dir="$(dirname "${BASH_SOURCE[0]}")"
 
+echo "Removing existing GraphQL docs..."
 rm -rf ${scripts_dir}/../pages/apis/graphql/schemas/*
+
+echo "Generating GraphQL docs..."
 ruby "${scripts_dir}/generate_graphql_api_content.rb"
