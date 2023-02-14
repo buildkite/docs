5db038db85279d90c04ef17669c3d09e7570a88d
cjros <chris.c@buildkite.com>
2023-02-15 10:52:36 +1100

    Remove optional chaining operator
    Fixes a failing build

--- app/assets/javascripts/toc.js
+++ app/assets/javascripts/toc.js
@@ -1,11 +1,11 @@
 "use strict";
 
-(function(window, document) {
+(function (window, document) {
   const initCurrentLinkListener = () => {
     const scrollPadding = 125;
-    const currentClassName = 'Toc__link--current';
-    const tocLinkNodes = document.querySelectorAll('.Toc__link');
-    const headingNodes = document.querySelectorAll('h2.Docs__heading');
+    const currentClassName = "Toc__link--current";
+    const tocLinkNodes = document.querySelectorAll(".Toc__link");
+    const headingNodes = document.querySelectorAll("h2.Docs__heading");
 
     const getDistanceFromTop = (element) => {
       if (!element.offsetParent) {
@@ -17,23 +17,25 @@
     const getHeadingPos = (heading) => getDistanceFromTop(heading);
 
     const setCurrentLink = (hash) => {
-      const currentNode = [ ...tocLinkNodes ].find((link) => link.hash === hash);
-      
       tocLinkNodes.forEach((link) => {
         link.classList.remove(currentClassName);
-        link.removeAttribute('aria-current');
+        link.removeAttribute("aria-current");
       });
-      currentNode?.classList.add(currentClassName);
-      currentNode?.setAttribute('aria-current', '');
+
+      const currentNode = [...tocLinkNodes].find((link) => link.hash === hash);
+      if (currentNode) {
+        currentNode.classList.add(currentClassName);
+        currentNode.setAttribute("aria-current", "");
+      }
     };
 
     tocLinkNodes.forEach((link) => {
-      link.addEventListener('click', (e) => setCurrentLink(e.target.hash));
+      link.addEventListener("click", (e) => setCurrentLink(e.target.hash));
     });
 
-    window.addEventListener('scroll', () => {
+    window.addEventListener("scroll", () => {
       const topPos = window.scrollY + scrollPadding;
-      
+
       headingNodes.forEach((heading) => {
         const pos = getHeadingPos(heading);
         if (topPos >= pos) {
@@ -44,16 +46,15 @@
   };
 
   const initToggle = () => {
-    const toggleNode = document.querySelector('.Toc__toggle');
-    const listNode = document.querySelector('.Toc__list');
+    const toggleNode = document.querySelector(".Toc__toggle");
+    const listNode = document.querySelector(".Toc__list");
 
-    toggleNode.addEventListener('click', (e) => {
-      e.target.classList.toggle('Toc__toggle--is-collapsed');
-      listNode.classList.toggle('Toc__list--is-collapsed');
+    toggleNode.addEventListener("click", (e) => {
+      e.target.classList.toggle("Toc__toggle--is-collapsed");
+      listNode.classList.toggle("Toc__list--is-collapsed");
     });
   };
 
   initCurrentLinkListener();
   initToggle();
-
 })(window, document);
