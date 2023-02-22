3bd895bb7cf44226ade0edf55bb2563a032f1d57
Oly Su <oly@buildkite.com>
2023-02-22 11:52:08 +1100

    ✨ Add responsive previews

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,43 @@
-import '../app/assets/stylesheets/application.scss'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import '../app/assets/stylesheets/application.scss';
+
+const docsViewports = {
+  screenXs: {
+    name: 'screen-xs',
+    styles: {
+      width: '480px',
+      height: '768px',
+    },
+  },
+  screenSm: {
+    name: 'screen-sm',
+    styles: {
+      width: '768px',
+      height: '1024px',
+    },
+  },
+  screenMd: {
+    name: 'screen-md',
+    styles: {
+      width: '960px',
+      height: '768px',
+    },
+  },
+  screenLg: {
+    name: 'screen-lg',
+    styles: {
+      width: '1280px',
+      height: '832px',
+    },
+  },
+  screenXl: {
+    name: 'screen-xl',
+    styles: {
+      width: '1536px',
+      height: '982px',
+    },
+  },
+}
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -8,4 +47,10 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+  viewport: {
+    viewports: { 
+      ...docsViewports,
+      ...INITIAL_VIEWPORTS,
+    },
+  },
+};
