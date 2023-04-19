d810c5089cd69fcec929a1cac6bf20627c1c2d97
Chris Campbell <chris.c@buildkite.com>
2023-04-19 11:32:05 +1000

    Keep track of current nav item based on request path
    Generate flat path map for nav items so we can highlight the current nav item
    and its ancestors.

--- app/models/nav.rb
+++ app/models/nav.rb
@@ -1,9 +1,51 @@
 class Nav
-  def initialize(nav_data = [])
-    @nav_data = nav_data
+  attr_reader :data, :path
+
+  def initialize(path, data = [])
+    @data = data.freeze
+    @path = path
   end
 
-  def nav_tree
-    @nav_data
+  # Returns the current nav item
+  def current_item
+    item = route_map[path.sub("/docs/", "")]
+    if !item
+      raise "Could not find nav item for #{path}"
+    end
+    item
+  end
+
+  # Returns a hash of routes, indexed by path
+  #
+  # @example
+  #   {
+  #     "path/to/page": {
+  #       path: "path/to/page",
+  #       parents: ["parent", "grandparent"]
+  #     }
+  #   }
+  def route_map
+    index_by_path = Proc.new do |route_map, items, parents|
+      items.each do |item|
+        route_map[item["path"]] = {
+          path: item["path"],
+          parents: parents
+        }
+
+        if item["children"].present?
+          index_by_path.call(
+            route_map,
+            item["children"],
+            parents.clone.push(item["name"]),
+          )
+        end
+      end
+    end
+
+    @_route_map ||= data.each_with_object({}) do |node, route_map|
+      if node["children"].present?
+        index_by_path.call(route_map, node["children"], [node["name"]], 0)
+      end
+    end
   end
 end
