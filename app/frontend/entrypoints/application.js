f42ea971cda5ffcf11dbb077374ed108ba02a3fd
Chris Campbell <chris.c@buildkite.com>
2023-04-20 23:31:26 +1000

    Remove uglifier compression

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -18,3 +18,16 @@ function render() {
   initToc();
   attachCopyToClipboardButton("pre.highlight");
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+  document.onkeydown = (e) => {
+    const event = window.event || e;
+
+    switch (event.keyCode) {
+      case 191:
+        e.preventDefault();
+        document.getElementById("search").focus();
+        break;
+    }
+  };
+});
