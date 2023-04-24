d65a3e041780e09b8a407d30b028e7af62ad92e5
Chris Campbell <chris.c@buildkite.com>
2023-04-24 10:42:09 +1000

    Remove `probably_authenticated`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  helper_method :probably_authenticated?
-
   def route_not_found
     render file: Rails.root.join("public","404.html"), layout: false, status: 404
   end
@@ -19,12 +17,5 @@ class ApplicationController < ActionController::Base
     payload[:request_id] = request.headers.fetch("X-Amzn-Trace-Id", request.request_id)
     payload[:remote_ip] = request.remote_ip
     payload[:user_agent] = request.user_agent
-  end
-
-
-  # When you login to Buildkite, we set this cookie as an indicator for other
-  # services that the user *may* be logged in.
-  def probably_authenticated?
-    request.cookie_jar[:bk_logged_in] == "true"
   end
 end
