952b55420b248a8995655c25d063daab9efcf9f3
Chris Campbell <chris.c@buildkite.com>
2023-05-02 11:10:24 +1000

    Set https://buildkite.com as sitemap host

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.example.com"
+SitemapGenerator::Sitemap.default_host = "https://buildkite.com"
 SitemapGenerator::Sitemap.sitemaps_path = "docs/"
 
 SitemapGenerator::Sitemap.create do
