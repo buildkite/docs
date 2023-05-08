76cc736ea89e847ee6951068de5181c43be15b0f
Chris Campbell <chris.c@buildkite.com>
2023-05-08 11:40:57 +1000

    Remove buildkite.com/ from docs sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,6 +1,7 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://buildkite.com"
 SitemapGenerator::Sitemap.sitemaps_path = "docs/"
+SitemapGenerator::Sitemap.include_root = false
 
 SitemapGenerator::Sitemap.create do
   Page.all.each do |page|
