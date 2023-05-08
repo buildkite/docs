c754ab0bc178a18fb2058e714bfeb526f4211318
Chris Campbell <chris.c@buildkite.com>
2023-05-08 11:41:23 +1000

    Spacify priority and changefrequency for sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -4,7 +4,9 @@ SitemapGenerator::Sitemap.sitemaps_path = "docs/"
 SitemapGenerator::Sitemap.include_root = false
 
 SitemapGenerator::Sitemap.create do
+  add "/docs", :changefreq => "weekly", :priority => 0.9
+
   Page.all.each do |page|
-    add page.path, :lastmod => page.updated_at
+    add page.path, :lastmod => page.updated_at, :changefreq => "monthly", :priority => 0.5
   end
 end
