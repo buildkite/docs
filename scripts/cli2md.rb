184625980849fee5eaf8e5b765696e5673549c75
Narthana Epa <narthana.epa@gmail.com>
2023-08-15 20:32:07 +1000

    No-op refactor of the cli2md script to silence rubocop warnings

--- scripts/cli2md.rb
+++ scripts/cli2md.rb
@@ -1,66 +1,64 @@
-require 'cgi'
+# frozen_string_literal: true
+
+require "cgi"
 
 incode = false
-
 first_param = false
 
+# read from stdin or files in the args
 ARGF.each_with_index do |line, line_num|
-    # Headings
-    if /^(\w*):/ =~ line
-        puts "### #{$1}"
-    # Initial usage command
-    elsif line_num == 2
-        puts "`#{line.strip}`"
-    # Code sections
-    elsif /\s{3}\$/ =~ line
-        # Break code lines that end in \
-        if line [-2] == "\\"
-            incode=true
-        end
-        puts " #{line}"
-    # If previous line ends in \ indent to code block
-    elsif incode == true
-        incode = false
-        puts "   #{line}"
-    # Lists of parameters
-    #    --config value                         Path to a configuration file [$BUILDKITE_AGENT_CONFIG]
-    elsif /\s{3}(-{2}[a-z0-9\- ]*)([A-Z].*)$/ =~ line
-        if(first_param==false)
-            puts "<!-- vale off -->\n\n<table class=\"Docs__attribute__table\">"
-            first_param = true
-        end
-        command_and_value = $1.rstrip
-        command = command_and_value.split[0][2..-1]
-        value = command_and_value.split[1]
-        desc    = $2
-
-        # Extract $BUILDKITE_* env and remove from desc
-        /(\$BUILDKITE[A-Z0-9_]*)/ =~ desc
-        env_var = $1
-        desc.gsub!(/(\s\[\$BUILDKITE[A-Z0-9_]*\])/,"")
-
-
-        # Wrap https://agent.buildkite.com/v3 in code
-        desc.gsub!('https://agent.buildkite.com/v3',"<code>https://agent.buildkite.com/v3</code>")
-
-        # Replace all prime symbols with backticks. We use prime symbols instead of backticks in CLI helptext for... reasons.
-        # See: https://github.com/buildkite/agent/blob/main/clicommand/prime-signs.md
-        desc.tr!('′', '`')
-        print "<tr id=\"#{command}\">"
-        print "<th><code>--#{command} #{value}</code> <a class=\"Docs__attribute__link\" href=\"##{command}\">#</a></th>"
-        print "<td><p>#{desc}"
-        unless env_var.nil? || env_var.empty?
-            print "<br /><strong>Environment variable</strong>: <code>#{env_var}</code>"
-        end
-        print "</p></td>"
-        print "</tr>"
-        puts
-    else
-        if(first_param==true)
-            puts "</table>\n\n<!-- vale on -->\n"
-            first_param = false
-            next
-        end
-        puts CGI::escapeHTML(line.lstrip)
+  # Headings
+  if /^(\w*):/ =~ line
+    puts "### #{Regexp.last_match(1)}"
+  # Initial usage command
+  elsif line_num == 2
+    puts "`#{line.strip}`"
+  # Code sections
+  elsif /\s{3}\$/ =~ line
+    # Break code lines that end in \
+    incode = true if line[-2] == "\\"
+    puts " #{line}"
+  # If previous line ends in \ indent to code block
+  elsif incode
+    incode = false
+    puts "  #{line}"
+  # Lists of parameters
+  #  --config value             Path to a configuration file [$BUILDKITE_AGENT_CONFIG]
+  elsif /\s{3}(-{2}[a-z0-9\- ]*)([A-Z].*)$/ =~ line
+    if first_param == false
+      puts "<!-- vale off -->\n\n<table class=\"Docs__attribute__table\">"
+      first_param = true
     end
+    command_and_value = Regexp.last_match(1).rstrip
+    command = command_and_value.split[0][2..]
+    value = command_and_value.split[1]
+    desc  = Regexp.last_match(2)
+
+    # Extract $BUILDKITE_* env and remove from desc
+    /(\$BUILDKITE[A-Z0-9_]*)/ =~ desc
+    env_var = Regexp.last_match(1)
+    desc.gsub!(/(\s\[\$BUILDKITE[A-Z0-9_]*\])/, "")
+
+    # Wrap https://agent.buildkite.com/v3 in code
+    desc.gsub!("https://agent.buildkite.com/v3", "<code>https://agent.buildkite.com/v3</code>")
+
+    # Replace all prime symbols with backticks. We use prime symbols instead of backticks in CLI
+    # helptext for... reasons.
+    # See: https://github.com/buildkite/agent/blob/main/clicommand/prime-signs.md
+    desc.tr!("′", "`")
+    print "<tr id=\"#{command}\">"
+    print "<th><code>--#{command} #{value}</code> <a class=\"Docs__attribute__link\" href=\"##{command}\">#</a></th>"
+    print "<td><p>#{desc}"
+    print "<br /><strong>Environment variable</strong>: <code>#{env_var}</code>" unless env_var.nil? || env_var.empty?
+    print "</p></td>"
+    print "</tr>"
+    puts
+  else
+    if first_param
+      puts "</table>\n\n<!-- vale on -->\n"
+      first_param = false
+      next
+    end
+    puts CGI.escapeHTML(line.lstrip)
+  end
 end
