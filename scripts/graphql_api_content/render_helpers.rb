35c93c1fb2829032b1c7bf71f616f8298c85e7c4
Chris Campbell <chris.c@buildkite.com>
2023-08-17 14:33:36 +1000

    Pull GraphQL description from first paragraph

--- scripts/graphql_api_content/render_helpers.rb
+++ scripts/graphql_api_content/render_helpers.rb
@@ -237,7 +237,7 @@ module RenderHelpers
       <!-- vale on -->
       #{valeOff ? "<!-- vale off -->" : nil}
 
-      #{render_html(schema_type_data["description"])}
+      #{schema_type_data["description"]}
 
       #{table}
 
