0fd25aa67318e49d802e7eca5b322b8b8e8cacee
Narthana Epa <narthana.epa@gmail.com>
2023-08-23 09:35:43 +1000

    Rewrite doc generation script
    Now, we will encase code blocks in `shell` backtick blocks.
    In the input, we will use 2 spaces for indentation, and we will use
    4 spaces to indicate the start of a code block.
    
    We also substitute prime symbols in more places. And we will prevent the
    user's home directory from leaking into the docs via this script.

--- scripts/cli2md.rb
+++ scripts/cli2md.rb
@@ -2,37 +2,51 @@
 
 require "cgi"
 
-incode = false
-first_param = false
+# are we in a code block?
+in_code = false
+
+# are with in the table of parameters?
+in_table = false
 
 # read from stdin or files in the args
 ARGF.each_with_index do |line, line_num|
-  # Headings
+  # Replace all prime symbols with backticks. We use prime symbols instead of backticks in CLI
+  # helptext because Go does not support escaping backticks in backtick delimited strings.
+  # See: https://github.com/buildkite/agent/blob/main/clicommand/prime-signs.md
+  line.tr!("′", "`")
+
+  # Some agent help texts dynamically replace $HOME with the current user's home directory.
+  # We need to replace it back for the docs
+  line.gsub!(Dir.home, "$HOME")
+
+  # Headings end in `:`
   if /^(\w*):/ =~ line
     puts "### #{Regexp.last_match(1)}"
   # Initial usage command
   elsif line_num == 2
     puts "`#{line.strip}`"
-  # Code sections
-  elsif /\s{3}\$/ =~ line
-    # Break code lines that end in \
-    incode = true if line[-2] == "\\"
-    puts " #{line}"
-  # If previous line ends in \ indent to code block
-  elsif incode
-    incode = false
-    puts "  #{line}"
+  # code blocks
+  elsif /^\s{4}/ =~ line
+    puts "```shell" unless in_code
+    puts line.gsub(/^\s{4}/, "")
+    in_code = true
+
+  # first line after a code block
+  elsif in_code
+    in_code = false
+    puts "```"
+    puts line
+
   # Lists of parameters
   #  --config value             Path to a configuration file [$BUILDKITE_AGENT_CONFIG]
-  elsif /\s{3}(-{2}[a-z0-9\- ]*)([A-Z].*)$/ =~ line
-    if first_param == false
-      puts "<!-- vale off -->\n\n<table class=\"Docs__attribute__table\">"
-      first_param = true
-    end
+  elsif /\s{2}(-{2}[a-z0-9\- ]*)([A-Z].*)$/ =~ line
+    puts %(<!-- vale off -->\n\n<table class="Docs__attribute__table">) unless in_table
+    in_table = true
+
     command_and_value = Regexp.last_match(1).rstrip
     command = command_and_value.split[0][2..]
     value = command_and_value.split[1]
-    desc  = Regexp.last_match(2)
+    desc = Regexp.last_match(2)
 
     # Extract $BUILDKITE_* env and remove from desc
     /(\$BUILDKITE[A-Z0-9_]*)/ =~ desc
@@ -42,24 +56,20 @@ ARGF.each_with_index do |line, line_num|
     # Wrap https://agent.buildkite.com/v3 in code
     desc.gsub!("https://agent.buildkite.com/v3", "<code>https://agent.buildkite.com/v3</code>")
 
-    # Replace all prime symbols with backticks. We use prime symbols instead of backticks in CLI
-    # helptext for... reasons.
-    # See: https://github.com/buildkite/agent/blob/main/clicommand/prime-signs.md
-    desc.tr!("′", "`")
-    desc.gsub!(Dir.home, "$HOME")
-    print "<tr id=\"#{command}\">"
-    print "<th><code>--#{command} #{value}</code> <a class=\"Docs__attribute__link\" href=\"##{command}\">#</a></th>"
+    print %(<tr id="#{command}">)
+    print %(<th><code>--#{command} #{value}</code> <a class="Docs__attribute__link" href="##{command}">#</a></th>)
     print "<td><p>#{desc}"
     print "<br /><strong>Environment variable</strong>: <code>#{env_var}</code>" unless env_var.nil? || env_var.empty?
     print "</p></td>"
     print "</tr>"
     puts
   else
-    if first_param
-      puts "</table>\n\n<!-- vale on -->\n"
-      first_param = false
-      next
-    end
     puts CGI.escapeHTML(line.lstrip)
   end
 end
+
+# last line of input was in a table
+puts "</table>\n\n<!-- vale on -->\n" if in_table
+
+# last line of input was in a code block
+puts "```" if in_code
