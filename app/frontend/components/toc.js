104f1e9b55ad01e1ff3c9f32ea56acc028ba666b
Chris Campbell <chris.c@buildkite.com>
2023-08-24 17:26:54 +1000

    Render svg path that follows visible sections

--- app/frontend/components/toc.js
+++ app/frontend/components/toc.js
@@ -1,5 +1,85 @@
 export function initToc() {
+  const nav = document.querySelector(".Toc");
+  const visibleClass = "Toc__list-item--visible";
+  const navPath = nav.querySelector("svg path");
+  const navListItems = [...document.querySelectorAll(".Toc__list-item")];
+
+  const items = navListItems.map((listItem) => {
+    const anchor = listItem.querySelector("a");
+    return { listItem, anchor };
+  });
+
+  function drawPath() {
+    let path = [];
+    let pathIndent;
+
+    items.forEach((item, i) => {
+      const x = item.anchor.offsetLeft + 3;
+      const y = item.anchor.offsetTop;
+      const height = item.anchor.offsetHeight;
+
+      if (i === 0) {
+        path.push("M", x, y, "L", x, y + height);
+        item.pathStart = 0;
+      } else {
+        if (pathIndent !== x) path.push("L", pathIndent, y);
+
+        path.push("L", x, y);
+
+        navPath.setAttribute("d", path.join(" "));
+        item.pathStart = navPath.getTotalLength() || 0;
+        path.push("L", x, y + height);
+      }
+
+      pathIndent = x;
+      navPath.setAttribute("d", path.join(" "));
+      item.pathEnd = navPath.getTotalLength();
+    });
+  }
+
+  function syncPath() {
+    const pathLength = navPath.getTotalLength();
+
+    let pathStart = pathLength;
+    let pathEnd = 0;
+    let lastPathStart, lastPathEnd;
+    let visibleItemFound = false;
+
+    items.forEach((item) => {
+      // Only make the first visible item active
+      item.listItem.classList.remove("active");
+      if (visibleItemFound) {
+        item.listItem.classList.remove("active");
+        return;
+      }
+
+      if (item.listItem.classList.contains(visibleClass)) {
+        item.listItem.classList.add("active");
+
+        pathStart = Math.min(item.pathStart, pathStart);
+        pathEnd = Math.max(item.pathEnd, pathEnd);
+        visibleItemFound = true;
+      }
+    });
+
+    if (visibleItemFound && pathStart < pathEnd) {
+      if (pathStart !== lastPathStart || pathEnd !== lastPathEnd) {
+        const dashArray = `1 ${pathStart} ${pathEnd - pathStart} ${pathLength}`;
+
+        navPath.style.setProperty("stroke-dashoffset", "1");
+        navPath.style.setProperty("stroke-dasharray", dashArray);
+        navPath.style.setProperty("opacity", 1);
+      }
+    } else {
+      navPath.style.setProperty("opacity", 0);
+    }
+
+    lastPathStart = pathStart;
+    lastPathEnd = pathEnd;
+  }
+
   const initCurrentLinkListener = () => {
+    drawPath();
     const content = document.querySelector(".Page");
 
     const observer = new IntersectionObserver(
@@ -7,19 +87,22 @@ export function initToc() {
         entries.forEach((entry) => {
           const id = entry.target.getAttribute("id");
 
-          const link = document.querySelector(`.Toc__link--h2[href="#${id}"]`);
+          const link = document.querySelector(`.Toc__link[href="#${id}"]`);
           if (entry.intersectionRatio > 0) {
-            link.parentElement.classList.add("Toc__list-item--current");
+            link.parentElement.classList.add(visibleClass);
           } else {
-            link.parentElement.classList.remove("Toc__list-item--current");
+            link.parentElement.classList.remove(visibleClass);
           }
         });
+
+        syncPath();
       },
       { rootMargin: `-${content.offsetTop}px 0px 0px 0px` }
     );
 
-    document.querySelectorAll("section[id]").forEach((sections) => {
-      observer.observe(sections);
+    drawPath();
+    document.querySelectorAll("section[id]").forEach((section) => {
+      observer.observe(section);
     });
   };
 
