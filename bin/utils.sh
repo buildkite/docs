10358b0402e451f7b544a58bfe7ce2e4e450962a
Chris Campbell <chris.c@buildkite.com>
2023-11-20 16:19:58 +1100

    Fix curl request for creating a pull request

--- bin/utils.sh
+++ bin/utils.sh
@@ -40,6 +40,6 @@ function create_pull_request() {
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $GH_TOKEN" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    --data "{\"title\":\"$1\", "body\":\"$2\"}" \
-    https://api.github.com/repos/buildkite/docs/pulls"
+    --data "{\"title\":\"$1\", \"body\":\"$2\", \"head\":\"$BRANCH\", \"base\":\"main\"}" \
+    "https://api.github.com/repos/buildkite/docs/pulls"
 }
