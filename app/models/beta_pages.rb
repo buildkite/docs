a7f777f91abab221edb55e95c834eeb267eb4e6d
Katie Wright <katie.wrght@gmail.com>
2023-11-30 16:41:45 +1300

    remove beta pills from nav and analytics api pages

--- app/models/beta_pages.rb
+++ app/models/beta_pages.rb
@@ -5,10 +5,6 @@ class BetaPages
       'test-analytics/swift-collectors',
       'test-analytics/android-collectors',
       'test-analytics/dotnet-collectors',
-      'apis/rest-api/analytics/flaky-tests',
-      'apis/rest-api/analytics/suites',
-      'apis/rest-api/analytics/runs',
-      'apis/rest-api/analytics/tests',
       'agent/clusters',
       'apis/rest-api/clusters',
       'apis/rest-api/pipeline-templates'
