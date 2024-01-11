c1499e4c6e2466b4367779b7f74303b91c46f561
Jason Hinchcliffe <hinchy@gmail.com>
2024-01-12 10:42:51 +1100

    feat: add 'beta' tags to new REST API menu items

--- app/models/beta_pages.rb
+++ app/models/beta_pages.rb
@@ -6,6 +6,11 @@ class BetaPages
       'apis/rest-api/pipeline-templates',
       'apis/rest-api/team-pipelines',
       'pipelines/cluster-queue-metrics',
+      'apis/rest-api/organizations/members',
+      'apis/rest-api/organizations/team-members',
+      'apis/rest-api/organizations/team-pipelines',
+      'apis/rest-api/organizations/team-suites',
+      'apis/rest-api/organizations/teams',
     ]
   end
 end
