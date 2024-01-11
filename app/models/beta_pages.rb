b6abfc40140628d86af8615459548cf508fc8f0e
Jason Hinchcliffe <hinchy@gmail.com>
2024-01-11 12:42:02 +1100

    feat: add Team Pipeline REST API documentation

--- app/models/beta_pages.rb
+++ app/models/beta_pages.rb
@@ -1,7 +1,11 @@
 class BetaPages
   def self.all
     [
-      'pipelines/cluster-queue-metrics'
+      'agent/clusters',
+      'apis/rest-api/clusters',
+      'apis/rest-api/pipeline-templates',
+      'apis/rest-api/team-pipelines',
+      'pipelines/cluster-queue-metrics',
     ]
   end
 end
