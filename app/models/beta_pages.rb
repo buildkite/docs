f54e99751d3eda4b527c69bea05f47b222b7033d
Giles Gaskell <giles@buildkite.com>
2024-06-13 16:02:33 +1000

    Reorder BetaPages to reflect docs structure.

--- app/models/beta_pages.rb
+++ app/models/beta_pages.rb
@@ -2,14 +2,14 @@ class BetaPages
   def self.all
     [
       'pipelines/cluster-queue-metrics',
+      'test-analytics/test-ownership',
       'packages',
       'apis/rest-api/team-pipelines',
       'apis/rest-api/organizations/members',
       'apis/rest-api/teams',
       'apis/rest-api/teams/members',
       'apis/rest-api/teams/pipelines',
-      'apis/rest-api/teams/suites',
-      'test-analytics/test-ownership'
+      'apis/rest-api/teams/suites'
     ]
   end
 end
