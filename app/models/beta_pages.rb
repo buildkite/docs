aaace5b6a0984640afa0ae7bd58e8381c1e6f261
Giles Gaskell <giles@buildkite.com>
2024-09-23 15:16:47 +1000

    Remove beta notices from Teams and Org member pages/areas of the REST API docs.

--- app/models/beta_pages.rb
+++ app/models/beta_pages.rb
@@ -2,13 +2,7 @@ class BetaPages
   def self.all
     [
       'pipelines/cluster-queue-metrics',
-      'test-analytics/test-ownership',
-      'apis/rest-api/team-pipelines',
-      'apis/rest-api/organizations/members',
-      'apis/rest-api/teams',
-      'apis/rest-api/teams/members',
-      'apis/rest-api/teams/pipelines',
-      'apis/rest-api/teams/suites'
+      'test-analytics/test-ownership'
     ]
   end
 end
