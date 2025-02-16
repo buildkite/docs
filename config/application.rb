92d3691656652747e060383ee2377608aa07bf4c
James Healy <james@buildkite.com>
2025-02-16 14:55:35 +1100

    Update default config to 6.1
    This is the version of rails we're running now/ Fortunately this app is
    quite simple, and this seems to still Just Work

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ Bundler.require(*Rails.groups)
 module Docs
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.1
 
     config.autoloader = :zeitwerk
 
