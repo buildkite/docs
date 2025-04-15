e2a04edbc71bf1ccbd9f8c5e6b196b6d6f2abcf1
Patrick Robinson <patrick@buildkite.com>
2025-04-15 10:11:36 +1000

    Don't send client errors to bugsnag, it contributes to our rate limit

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,5 +1,13 @@
 if ENV["BUGSNAG_API_KEY"].present?
   Bugsnag.configure do |config|
     config.api_key = ENV["BUGSNAG_API_KEY"]
+
+    # Ignore exceptions handled as a client error by ActionDispatch::ShowExceptions
+    config.add_on_error(-> (event) {
+      name = event.original_error.class.name
+      if ActionDispatch::ExceptionWrapper.status_code_for_exception(name) < 500
+        return false # discard
+      end
+    })
   end
 end
