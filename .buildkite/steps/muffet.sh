782b406b9c9aac3bd232e74c9d964f090f8f3621
Giles Gaskell <giles@buildkite.com>
2025-09-19 15:50:48 +1000

    Add instruction for 'timeout' statuses and Artifactory link updates.

--- .buildkite/steps/muffet.sh
+++ .buildkite/steps/muffet.sh
@@ -101,10 +101,11 @@ else
         echo
         echo "Before looking at the list of links below to work out what's going on, ignore links with **429** or **403** statuses first. Links returning either of these two statuses will likely work when selected by a human. Also, muffet's been configured to allow this job to pass if all remaining links have statuses that are either only **429** or **403**."
         echo
-        echo "Instead, identify genuine link issues, such as those with a **404** status (not found) or ones returning an **id #some-fragment not found** issue, and resolve them. For **id #some-fragment not found** issues, fix the link and its fragment first (since the target content may have moved, or the link and its fragment might just happen to be wrong). However, if the correct link (which you manually tested yourself) is implemented and this job still fails, you'll likely need to add this correct link's full URL (excluding any query parameters from `?` onwards, but retaining its fragment) to the list of `--exclude` options in the `muffet.sh` script."
+        echo "Instead, identify genuine link issues, such as those with a **404** status (not found) or ones returning an **id #some-fragment not found** issue, and resolve them. For **id #some-fragment not found** issues, fix the link and its fragment first (since the target content may have moved, or the link and its fragment might just happen to be wrong). However, if the correct link (which you manually tested yourself) is implemented and this job still fails, you'll likely need to add this correct link's full URL (excluding any query parameters from ``?`` onwards, but retaining its fragment) to the list of ``--exclude`` options in the ``muffet.sh`` script."
         echo
-        echo "If you've added an `--exclude` entry for a link that generates an **id #some-fragment not found** error, but this job still fails with the same error (that is, the link and its fragment actually works but muffet still reports it as erroneous), then remove the fragment part of the URL from its `--exclude` entry."
+        echo "If you've added an ``--exclude`` entry for a link that generates an **id #some-fragment not found** error, but this job still fails with the same error (that is, the link and its fragment actually works but muffet still reports it as erroneous), then remove the fragment part of the URL from its ``--exclude`` entry."
         echo
+        echo "Last, if you see any links with a **timeout** status, just re-run the muffet job again."
     } >> annotation.md
 
     < muffet-results.json jq -r "$jq_query" >> annotation.md
