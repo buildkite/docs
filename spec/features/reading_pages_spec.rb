308f4bf92f951fc8b0adcd88dcce9d6ac941d8f8
Giles Gaskell <giles@buildkite.com>
2026-02-17 15:24:25 +1100

    Fix RSpec tests.

--- spec/features/reading_pages_spec.rb
+++ spec/features/reading_pages_spec.rb
@@ -16,12 +16,12 @@ RSpec.feature "reading pages" do
 
     it "has appropriate meta tags" do
       visit "/docs/agent"
-      expect(page.find(%{meta[property="og:title"]}, visible: false)[:content]).to eql("The Buildkite agent v3")
+      expect(page.find(%{meta[property="og:title"]}, visible: false)[:content]).to eql("The Buildkite agent")
     end
 
     it "has the correct title" do
       visit "/docs/agent/v3"
-      expect(page.title).to include("The Buildkite Agent")
+      expect(page.title).to include("The Buildkite agent")
     end
 
     it "links to the GitHub source files" do
